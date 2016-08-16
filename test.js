d087bc7b00c336a1a2eed95486a9582865bb50cb
RaynoldKim <raynoldkim@gmail.com>
2016-08-17 01:05:49 +0900

    git initial test

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
 var request = require('supertest');
 var app = require('./app');
 
-request(app)
-	.get('/')
-	.expect(200)
-	.end(function(err) {
-		if(err) throw err;
-		console.log('Done');
+describe('Requests to the root path', function(){
+
+	it('Returns a 200 status code', function(done){
+
+		request(app)
+			.get('/')
+			.expect(200)
+			.end(function(err) {
+				if(err) throw err;
+				done();
+			});
+
 	});
+
+
+});
