e47b3e83c9f2e33f87d323c9dd68ee482a301bde
RaynoldKim <raynoldkim@gmail.com>
2016-08-19 01:42:42 +0900

    Replace in memory with redis

--- app.js
+++ app.js
@@ -5,20 +5,42 @@ var urlencode = bodyParser.urlencoded( {extended: false});
 
 app.use(express.static('public'));
 
-var cities = {
-	'Lotopia' : 'some descrioption',
-	'Caspiana' : 'description',
-	'Indigo' : 'descript bla'
-};
+//Redis connection
+var redis = require('redis');
+if (process.env.REDISTOGO_URL) {
+
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var client = redis.createClient(rtg.port, rtg.hostname);
+	client.auth(rtg.auth.split(":")[1]);
+
+} else {
+	
+	var client = redis.createClient();
+}
+
+client.select((process.env.NODE_ENV || 'development').length);
+
+//End Redis Connection
+
+// client.hset('cities', 	'Lotopia' , 'some descrioption');
+// client.hset('cities', 	'Caspiana' , 'description');
+// client.hset('cities', 	'Indigo' , 'descript bla');
+
 
 app.get('/cities', function(request, response){
-	response.json( Object.keys(cities));
+	client.hkeys('cities', function(error, names){
+		response.json(names);	
+	});
 });
 
 app.post('/cities', urlencode, function(request, response){
 	var newCity = request.body;
-	cities[newCity.name] = newCity.description;
-	response.status(201).json(newCity.name);
+
+	client.hset('cities', newCity.name, newCity.description, function(error){
+		if(error) throw error;
+		response.status(201).json(newCity.name);
+	});
+	
 });
 
 // app.listen(3000, function(){
