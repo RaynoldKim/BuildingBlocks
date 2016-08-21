373c15fb8fd897854c68e8ace05260bad60f2207
RaynoldKim <raynoldkim@gmail.com>
2016-08-21 23:53:43 +0900

    add show and extract routes

--- app.js
+++ app.js
@@ -1,75 +1,11 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var urlencode = bodyParser.urlencoded( {extended: false});
 
 app.use(express.static('public'));
 
-//Redis connection
-var redis = require('redis');
-if (process.env.REDISTOGO_URL) {
+var cities = require('./routes/cities');
 
-    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-	var client = redis.createClient(rtg.port, rtg.hostname);
-	client.auth(rtg.auth.split(":")[1]);
-
-} else {
-	
-	var client = redis.createClient();
-	client.select((process.env.NODE_ENV || 'development').length);
-
-}
+app.use('/cities', cities);
 
 
-//End Redis Connection
-
-// client.hset('cities', 	'Lotopia' , 'some descrioption');
-// client.hset('cities', 	'Caspiana' , 'description');
-// client.hset('cities', 	'Indigo' , 'descript bla');
-
-
-app.get('/cities', function(request, response){
-	client.hkeys('cities', function(error, names){
-		response.json(names);	
-	});
-});
-
-app.post('/cities', urlencode, function(request, response){
-	var newCity = request.body;
-
-	if(!newCity.name || !newCity.description){
-		response.sendStatus(400);
-		return false;
-	}
-
-	client.hset('cities', newCity.name, newCity.description, function(error){
-		if(error) throw error;
-		response.status(201).json(newCity.name);
-	});
-	
-});
-
-app.delete('/cities/:name', function(request, response){
-	client.hdel('cities', request.params.name, function(error){
-		if(error) throw error;
-		response.sendStatus(204);
-	});
-});
-
-app.get('/cities/:name', function(request, response){
-	client.hget('cities', request.params.name, function(error, description){
-		response.render('show.ejs', 
-			{ 
-				city:{
-					name: request.params.name, 
-					description: description
-				}
-			});
-	});
-});
-
-
-// app.listen(3000, function(){
-// 	console.log('Listening on port 3000');
-// });
 module.exports = app;
